10f5d3b76fd719eaf4d52107febaca8052adeecf
takeshi.ootsuka <takeshi.otsuka111@gmail.com>
2021-01-16 16:38:29 +0900

    Initial Commit

---
+++ SlideshowApp_21_01_16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp_21_01_16
+//
+//  Created by user on 2021/01/16.
+//  Copyright © 2021 takeshi-2983. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
